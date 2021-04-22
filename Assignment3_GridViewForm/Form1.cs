f43a0a8f01c9017708dec8abd10187a20dda69fa
Janavi Girase <67533673+Janavi658@users.noreply.github.com>
2021-04-22 23:26:21 +0530

    Add files via upload

---
+++ Assignment3_GridViewForm/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticeWindowsForm
+{
+    public partial class Form1 : Form
+    {
+        private List<Student> lstStudent = new List<Student>();
+        Student objStudent = new Student();
+        DataTable dt = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataColumn colName = new DataColumn("Name", typeof(string));
+            dt.Columns.Add(colName);
+            DataColumn colEmailID = new DataColumn("EmailID", typeof(string));
+            dt.Columns.Add(colEmailID);
+            DataColumn colEducation = new DataColumn("Education", typeof(string));
+            dt.Columns.Add(colEducation);
+            DataColumn colBranch = new DataColumn("Branch", typeof(string));
+            dt.Columns.Add(colBranch);
+            DataColumn colMobileNumber = new DataColumn("MobileNumber", typeof(string));
+            dt.Columns.Add(colMobileNumber);
+            DataColumn colLanguages = new DataColumn("Languages", typeof(string));
+            dt.Columns.Add(colLanguages);
+            DataColumn colWebDevelopment = new DataColumn("Web Development", typeof(string));
+            dt.Columns.Add(colWebDevelopment);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button_submit_Click(object sender, EventArgs e)
+        {
+            GridStudent.DataSource = null;
+            GridStudent.Refresh();
+            GridStudent.DataSource = dt;
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            objStudent.Name = textName.Text;
+            objStudent.EmailID = textEmailID.Text;
+            objStudent.Education = textEducation.Text;
+            objStudent.Branch = textBranch.Text;
+            objStudent.MobileNumber = textMobileNumber.Text;
+
+            DataRow row = dt.NewRow();
+            row["Name"] = textName.Text;
+            row["EmailID"] = textEmailID.Text;
+            row["Education"] = textEducation;
+            row["Branch"] = textBranch.Text;
+            row["MobileNumber"] = textMobileNumber.Text;
+            dt.Rows.Add(row);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+            objStudent.Languages = textLanguages.Text;
+            objStudent.WebDevelopment = textWebDevelopment.Text;
+            DataRow row = dt.NewRow();
+            row["Languages"] = textLanguages.Text;
+            row["Web Development"] = textWebDevelopment.Text;
+            dt.Rows.Add(row);
+        }
+    }
+}
---
+++ Assignment3_GridViewForm/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeWindowsForm
+{
+    public class Student
+    {
+        public string Name;
+        public string EmailID;
+        public string Education;
+        public string Branch;
+        public string MobileNumber;
+        public string Languages;
+        public string WebDevelopment;
+
+
+    }
+}
